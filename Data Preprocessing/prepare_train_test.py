576abd2885d9025a62dfcb8f4432d15f7aa944ba
Juan <juan@pxhealthcare.com>
2024-03-13 18:59:27 +0100

    Clean-up before initial run of preprocessing

--- Data Preprocessing/prepare_train_test.py
+++ Data Preprocessing/prepare_train_test.py
@@ -16,14 +16,16 @@ set_config(display='diagram', transform_output='pandas')
 RANDOM_SEED = 8
 
 def create_train_test_split(parent_dir=PARENT_DIR, test_size=0.3):
-    """Applies some initial filtering, splits into train and test. Takes as input original application_train.csv.zip, saves as output train/test_raw_apps.csv.zip"""
+    """
+    Applies filtering at row level, splits into train and test. Takes as input original application_train.csv.zip, saves as output train/test_raw_apps.csv.zip
+    """
     df = pd.read_csv(parent_dir / 'application_train.csv.zip', compression='zip')
     df.set_index("SK_ID_CURR", inplace=True)
 
-    # Filter out rows without an annuity - to homogenise the dataset
-    n_without_annuity = sum(pd.isna(df.AMT_ANNUITY))
-    print(f"Observations without annuity dropped {n_without_annuity}")
-    df = df.loc[~pd.isna(df.AMT_ANNUITY), ]
+    # Filter out rows without goods price - to homogenise the dataset
+    n_without_goods_price = sum(pd.isna(df.AMT_GOODS_PRICE))
+    print(f"Observations without price of goods dropped {n_without_goods_price}")
+    df = df.loc[~pd.isna(df.AMT_GOODS_PRICE), ]
 
     # After filtering, apply train/test split, to avoid data leak
     sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=RANDOM_SEED)
@@ -45,11 +47,14 @@ def create_train_test_split(parent_dir=PARENT_DIR, test_size=0.3):
     train.to_csv(parent_dir / 'processed' / 'train_raw_apps.csv.zip')
     test.to_csv(parent_dir / 'processed' / 'test_raw_apps.csv.zip')
 
-    return df, train_index, test_index
+    return df, train, test, train_index, test_index
 
 
-def feature_eng(df, train_idx, test_idx, parent_dir=PARENT_DIR):
-     # APPLICATION-LEVEL FEATURE ENG
+def feature_eng(df, train, test, train_idx, test_idx, parent_dir=PARENT_DIR):
+    """
+    Builds features (at application, applicant level, the latter using `add_features`). Applies non data-based imputation, plus re-categorise of HOUR_APPR_PROCESS_START. Takes as input train/test_raw_apps.csv.zip, saves as output train/test_apps_ext.csv.zip
+    """
+    # FEATURE ENGINEERING - APPLICATION LEVEL
     df["LTV"] = df["AMT_CREDIT"] / df["AMT_GOODS_PRICE"]
     df["LB_Credit_Length"] = df["AMT_CREDIT"] / df["AMT_ANNUITY"]
     df["Credit_to_Inc"] = df["AMT_CREDIT"] / df["AMT_INCOME_TOTAL"]
@@ -62,10 +67,12 @@ def feature_eng(df, train_idx, test_idx, parent_dir=PARENT_DIR):
     # check - it works
     # missing_bureau = df[bureau_cols].isna().sum()
 
-    # Impute own car age also with zeros, when nan
-    # Before that, if nan, create separate dummy indicating that has no own car
-    df['OWN_CAR'] = pd.to_numeric(df['OWN_CAR_AGE'].isna())
-    df.loc[pd.isna(df['OWN_CAR_AGE']), 'OWN_CAR_AGE'] = 0
+    # Impute own car age as zero when not having a car
+    # relative frequencies - almost all missing in own car age do not have a car, except 5 obs
+    # have_car_by_car_age = train.groupby(["FLAG_OWN_CAR", "OWN_CAR_AGE"], dropna=False).size().reset_index()
+    df.loc[
+        (df.FLAG_OWN_CAR == "N") & pd.isna(df['OWN_CAR_AGE']), 
+        'OWN_CAR_AGE'] = 0
 
     # Re-categorise Hour appr process
     # Define function to map hour to time of day
@@ -87,14 +94,14 @@ def feature_eng(df, train_idx, test_idx, parent_dir=PARENT_DIR):
     max_missing = .51
     include = ["EXT_SOURCE_1"]
     number_cols_to_drop= len(df.columns[df.isna().mean() > max_missing]) - len(include)
-    # TODO: print this number!
+    print(f"Number of columns dropped given more than {100*max_missing} % missing: {number_cols_to_drop}")
     cut_df = df.copy()
     cut_df = cut_df.loc[:, df.columns[df.isna().mean() <= max_missing]]
     # add back columns in `include` vector
     cut_df[include] = df[include]
     df = cut_df
 
-    # Engineer features
+    # FEATURE ENGINEERING - APPLICANT LEVEL
     df = add_features(df)
     # Check - do engineered features have any missing?
     print(df.iloc[:, -20:].describe())
@@ -109,33 +116,30 @@ def feature_eng(df, train_idx, test_idx, parent_dir=PARENT_DIR):
     return df, train, test
 
 
-if __name__ == "__main__":
-    # Commented out to save time
-    # Note - these are saving outputs
-    # df, train_idx, test_idx = create_train_test_split()
-    # df, train, test = feature_eng(df, train_idx, test_idx)
-
-    train = pd.read_csv(PARENT_DIR / 'processed' / 'train_apps_ext.csv.zip', compression="zip", index_col=CURRENT_ID)
-    test = pd.read_csv(PARENT_DIR / 'processed' / 'test_apps_ext.csv.zip', compression="zip", index_col=CURRENT_ID)
-
-    ### DATA-BASED IMPUTATION - also dealing with missing for quantitative variables
-
+def data_based_imputation(train, test, parent_dir=PARENT_DIR):
+    """
+    DATA-BASED IMPUTATION - also dealing with missing for quantitative variables. Takes as input train/test_raw_apps.csv.zip, saves output as train/test_apps_ml.csv.zip
+    """
     # Split into X, y
     X_train, y_train = train.loc[:, train.columns != 'TARGET'], train.TARGET
     X_test, y_test = test.loc[:, test.columns != 'TARGET'], test.TARGET
 
     # Use metadata to categorise correctly
-    metadata = pd.read_csv(PARENT_DIR / "meta" / "train_summary_applications.csv", index_col=["Attribute"])
+    metadata = pd.read_csv(parent_dir / "meta" / "train_summary_applications.csv", index_col=["Attribute"])
     data_types = metadata["Data Type"]
     pd_cols = set(train.columns)
     numeric_features = [attr for attr, type in data_types.items() if type == "Quantitative" and attr in pd_cols]
     categorical_features = [attr for attr, type in data_types.items() if type == "Categorical" and attr in pd_cols]
-    # NOTE: applicant-level engineered features checked to have no missing. Otherwise, add here as needed
-    app_numeric_feats = ["LTV", "LB_Credit_Length", "Credit_to_Inc"]
-    hist_numeric_feats = ["CC_PROP_CONS_QRT", "CC_PROP_CONS_"]
-    numeric_features += app_numeric_feats
+
+    # NOTE: add to these lists engineered features as needed
+    application_numeric_feats = ["LTV", "LB_Credit_Length", "Credit_to_Inc"] 
+    applicant_numeric_feats = ["CC_PROP_CONS_CURR", "CC_PROP_CONS_QRT"]
+    extra_cat_feats = ['DAYTIME_PROCESS_START']
+
+    numeric_features += application_numeric_feats + applicant_numeric_feats
     # add daytime to categorical
-    categorical_features += ['DAYTIME_PROCESS_START']
+    categorical_features += extra_cat_feats
+
 
     # Create Column Transformer, based on numeric and categorical transformers
     numeric_transformer = Pipeline(steps=[
@@ -143,7 +147,7 @@ if __name__ == "__main__":
     ])
     categorical_transformer = Pipeline(steps=[
         ('onehot', OneHotEncoder(
-            drop='first', handle_unknown='ignore', sparse_output=False, min_frequency=.05
+            drop='first', handle_unknown='infrequent_if_exist', sparse_output=False, min_frequency=.05
             ))
     ])
     preprocessor_tree = ColumnTransformer(
@@ -157,8 +161,24 @@ if __name__ == "__main__":
     X_train_proc = preprocessor_tree.fit_transform(X_train)
     X_test_proc = preprocessor_tree.transform(X_test)
 
-    # TODO: Save processed
-    set_trace()
+    # Recreate train, test
+    train = X_train_proc
+    train["TARGET"] = y_train
+    test = X_test_proc
+    test["TARGET"] = y_test
+
+    train.to_csv(parent_dir / 'processed' / 'train_apps_ml.csv.zip')
+    test.to_csv(parent_dir / 'processed' / 'test_apps_ml.csv.zip')
+
+
+if __name__ == "__main__":
+    # NOTE - these functions have side effects - saving outputs in parent_dir (PARENT_DIR by default)
+    df, train, test, train_idx, test_idx = create_train_test_split()
+    df, train, test = feature_eng(df, train, test, train_idx, test_idx)
+    # Used to save running time
+    # train = pd.read_csv(PARENT_DIR / 'processed' / 'train_apps_ext.csv.zip', compression="zip", index_col=CURRENT_ID)
+    # test = pd.read_csv(PARENT_DIR / 'processed' / 'test_apps_ext.csv.zip', compression="zip", index_col=CURRENT_ID)
+    data_based_imputation(train, test)
 
 
 
