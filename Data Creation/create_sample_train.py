f9dca6edccfbca549670f7e6b5da35b84be16cba
Juan <juan@pxhealthcare.com>
2024-06-04 18:16:32 +0200

    Delete old directory names

--- Data Creation/create_sample_train.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from utils import PARENT_DIR
-
-df = pd.read_csv(PARENT_DIR / 'processed' / 'train_raw_apps.csv.zip', compression="zip")
-sample_df = df.sample(n = 10000, random_state=1234)
-df.to_csv(PARENT_DIR / 'processed' / 'sample_train_raw_apps.csv.zip', index=False)
