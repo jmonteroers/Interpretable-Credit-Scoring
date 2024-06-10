04bde21e5f6d40a01b5a9a4bfa93f5f1cd4becc2
Juan <juan@pxhealthcare.com>
2024-06-10 20:37:03 +0200

    Update WOE chart with new WOE refinements

--- reporting/woe_chart.py
+++ reporting/woe_chart.py
@@ -9,7 +9,8 @@ logger = logging.getLogger("WOE_logger")
 logging.basicConfig(level=logging.INFO)
 
 RANDOM_SEED = 1234
-
+DEFAULT_MIN_PREBIN_SIZE = 0.05
+DEFAULT_MAX_PVAL = 0.005
 
 df = pd.read_csv(PARENT_DIR / 'processed' / 'train_apps_imp.csv.gz', compression="gzip")
 
@@ -19,7 +20,8 @@ y = df[TARGET].values
 optb = OptimalBinning(
     name="LTV", dtype="numerical", solver="cp", 
     monotonic_trend="auto_asc_desc",
-    min_prebin_size=0.01,
+    min_prebin_size=DEFAULT_MIN_PREBIN_SIZE,
+    max_pvalue=DEFAULT_MAX_PVAL,
     random_state=RANDOM_SEED
     )
 optb.fit(x, y)
@@ -34,7 +36,7 @@ binning_table.plot(metric="woe", show_bin_labels=True)
 plt.show()
 
 # Comparison vs existing WoE - same!
-woe_mapping = pd.read_excel(PARENT_DIR / 'meta' / 'woe_mapping.xlsx')
+woe_mapping = pd.read_excel(PARENT_DIR / 'meta' / 'woe_map' / 'woe_mapping.xlsx')
 woe_mapping = woe_mapping.loc[woe_mapping["Attribute"] == "LTV", ]
 print(woe_mapping)
 
