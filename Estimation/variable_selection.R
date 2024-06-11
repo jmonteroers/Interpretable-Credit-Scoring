dc759d2b5f597fa947de8a64e39c88dffbeba2b3
Juan <juan@pxhealthcare.com>
2024-06-11 14:00:25 +0200

    Clean up variable selection code, refactor main action into variable_selection function

--- Estimation/variable_selection.R
+++ Estimation/variable_selection.R
@@ -1,59 +1,15 @@
 # Implement forward and bidirectional variable selection using StepAIC. 
 # Input: Dataset after IV selection
-# Output: Selected Model for Scorecard Estimation (_step)
-# It assumes that the directory has been changes to the path of the input
+# Output: Selected Models for Scorecard Estimation (_aic, _bic)
+# It assumes that the directory has been changed to the input path
 library(tictoc)
 library(car)  # for VIF selection
+library(dplyr)
 
-setwd("C:/Users/jmont/Documents/courses/msc-data-science/master-thesis/credit-scoring/TFM/Data/Home Credit/processed")
+RUN_AIC <- T
+RUN_BIC <- T
 
-# Load dataset
-train <- read.csv(unz("train_apps_iv.csv.zip", "train_apps_iv.csv"))
-train <- subset(train, select=-SK_ID_CURR)
-
-# Construct the formula for full model (for upper scope)
-indep_vars <- colnames(train)[2:ncol(train)]
-formula_full <- as.formula(
-  paste("~", paste(indep_vars, collapse = " + ")))
-
-# AIC
-
-# Backward Selection
-# full_model <- glm(TARGET ~ ., data = train, family = binomial)
-# fit_backward <- stepAIC(
-#   full_model, direction = "backward", trace = T)
-
-# Forward Selection
-# null_model <- glm(TARGET ~ 1, data = train, family = binomial)
-# tic("Forward Selection")
-# fit_forward <- step(
-#   null_model, direction = "forward",
-#   scope=list(lower = ~ 1, upper = formula_full), trace = 1,
-#   test = "F"
-#   )
-# toc()
-
-# Bidirectional Selection
-# tic("AIC, Bidirectional Selection")
-# null_model <- glm(TARGET ~ 1, data = train, family = binomial)
-# aic_fit_both <- step(
-#   null_model, direction = "both", 
-#   scope=list(lower = ~ 1, upper = formula_full), trace = 1,
-#   test = "F"
-# )
-# toc()
-
-# BIC
-k_bic <- log(nrow(train))
-tic("BIC, Bidirectional Selection")
-null_model <- glm(TARGET ~ 1, data = train, family = binomial)
-bic_fit_both <- step(
-  null_model, direction = "both", 
-  scope=list(lower = ~ 1, upper = formula_full), trace = 1,
-  test = "F", k = k_bic
-)
-toc()
-
+## Util functions ##
 # Extract selected variables from the fitted model object
 get_selected_variables <- function(fit) {
   selected_vars <- names(coef(fit))
@@ -61,31 +17,6 @@ get_selected_variables <- function(fit) {
   selected_vars <- selected_vars[selected_vars != "(Intercept)"]
   return(selected_vars)
 }
-
-# sel_vars_aic_both <- get_selected_variables(aic_fit_both)
-sel_vars_bic_both <- get_selected_variables(bic_fit_both)
-
-# Export train dataset with selected variables
-# train_aic <- train[, c("TARGET", sel_vars_aic_both)]
-# write.csv(train_aic, file=gzfile("train_apps_aic.csv.gz"), row.names = F)
-
-train_bic <- train[, c("TARGET", sel_vars_bic_both)]
-write.csv(train_bic, file=gzfile("train_apps_bic.csv.gz"), row.names = F)
-
-# VIC selection
-
-# to check number of rows of resulting scorecard
-get_nrows_scorecard <- function(df) {
-  nunique_per_col <- apply(train_bic, 2, function(c) length(unique(c)))
-  # substract number of target values
-  sum(nunique_per_col) - length(unique(df$TARGET))
-}
-
-# BIC
-train_bic <- read.csv(gzfile("train_apps_bic.csv.gz"))
-bic_model <- glm(TARGET ~ ., data = train_bic, family = binomial)
-# No VIF issues
-vif_bic <- vif(bic_model)
 
 # Remove positive coefficients, return dataset without columns that produce
 # positive coefficients in regression
@@ -102,7 +33,7 @@ remove_pos_coeffs <- function(data) {
       # remove column with positive coefficients
       print(paste("Dropping column", colnames(X_cleaned)[idx_pos]))
       X_cleaned <- X_cleaned[-idx_pos]
-      data <- cbind(train_bic$TARGET, X_cleaned)
+      data <- cbind(data$TARGET, X_cleaned)
       colnames(data)[1] <- "TARGET"
     } else {
       cleaned_data <- T
@@ -112,12 +43,62 @@ remove_pos_coeffs <- function(data) {
   list(data = data, last.fit = this_model)
 }
 
-cleaned_train_bic <- remove_pos_coeffs(train_bic)
-write.csv(
-  cleaned_train_bic$data, file=gzfile("train_apps_bic_npos.csv.gz"), row.names = F
+variable_selection <- function(train, k, outname){
+  # Construct the formula for full model (for upper scope)
+  indep_vars <- colnames(train)[2:ncol(train)]
+  formula_full <- as.formula(
+    paste("~", paste(indep_vars, collapse = " + ")))
+  
+  # Variable Selection - both directions
+  null_model <- glm(TARGET ~ 1, data = train, family = binomial)
+  fit_both <- step(
+    null_model, direction = "both", 
+    scope=list(lower = ~ 1, upper = formula_full), trace = 1, k = k
+    )
+  sel_vars_both <- get_selected_variables(fit_both)
+  sel_train <- train[, c("TARGET", sel_vars_both)]
+  sel_model <- glm(TARGET ~ ., data = sel_train, family = binomial)
+  
+  # VIF Selection
+  vif <- vif(sel_model)
+  attrs_large_vif <- names(vif[vif > 10.])
+  sel_train <- sel_train %>% select(-all_of(attrs_large_vif))
+  
+  # Removal, positive coefficients (in a loop)
+  sel_train <- remove_pos_coeffs(sel_train)
+  print(paste(
+    "Number of rows in resulting scorecard",
+    get_nrows_scorecard(sel_train$data)
+  ))
+  
+  # Output
+  write.csv(
+    sel_train$data, file=gzfile(outname), row.names = F
   )
-print(paste(
-  "Number of rows in resulting scorecard",
-  get_nrows_scorecard(cleaned_train_bic)
-  )
-)
+}
+
+# to find number of rows of resulting scorecard
+get_nrows_scorecard <- function(df) {
+  nunique_per_col <- apply(df, 2, function(c) length(unique(c)))
+  # substract number of target values
+  sum(nunique_per_col) - length(unique(df$TARGET))
+}
+
+## Load dataset ##
+train <- read.csv(unz("train_apps_iv.csv.zip", "train_apps_iv.csv"))
+train <- subset(train, select=-SK_ID_CURR)
+
+## AIC - Bidirectional Selection ##
+if (RUN_AIC) {
+  tic("AIC, Bidirectional Selection")
+  variable_selection(train, 2., "train_apps_aic.csv.gz")
+  toc()
+}
+
+## BIC - Bidirectional Selection ##
+if (RUN_BIC) {
+  k_bic <- log(nrow(train))
+  tic("BIC, Bidirectional Selection")
+  variable_selection(train, k_bic, "train_apps_bic.csv.gz")
+  toc()
+}
